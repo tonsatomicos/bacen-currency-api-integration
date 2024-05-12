d4a4d970642dc8732f9db944c9b4727ea7fab9d6
Hubris <soc141s.lm@gmail.com>
2024-05-12 18:52:05 -0300

    1

--- src/sql/create-table.sql
+++ src/sql/create-table.sql
@@ -3,6 +3,6 @@ CREATE TABLE ptax (
 	indicador VARCHAR, 
 	valor_dia FLOAT, 
 	dt_cadastro TIMESTAMP
-)
+);
 
 ALTER TABLE public.ptax ADD CONSTRAINT ptax_unique_constraint UNIQUE (sk_data, indicador);
